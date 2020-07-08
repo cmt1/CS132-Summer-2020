063233022dd9096d186a412dbeb080a01aa0ae53
cmt1 <cmt1@protonmail.com>
2020-07-07 21:34:54 -0700

    HOP-01-3

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Carlos";
+            int Age = 30;
+            bool isFemale = false;
+            double MyFloat = 10.1;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isFemale);
+            Console.WriteLine(MyFloat);
+        }
+        
+
+            
+            
+    }
+}
