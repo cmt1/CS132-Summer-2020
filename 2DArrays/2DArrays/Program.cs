64566bcac76342212763422a98435cc4c7872044
cmt1 <cmt1@protonmail.com>
2020-07-14 21:58:28 -0700

    Module 2

--- 2DArrays/2DArrays/Program.cs
+++ 2DArrays/2DArrays/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace _2DArrays
 {
     class Program
@@ -7,9 +8,26 @@ namespace _2DArrays
         static void Main(string[] args)
         {
             int rows = 3;
-            int colums = 3;
+            int columns = 3;
 
-            Console.WriteLine("Hello World!");
+            int[,] table = new int[rows, columns]; //create a 2D array tha is 3x3 (3 rows and 3 columns)
+            table[0, 0] = 1; //asign value to wach array element, no specific value is ExtenderProvidedPropertyAttribute, by default its 0
+            table[0, 1] = 2;
+            table[0, 2] = 3;
+            table[1, 1] = 5;
+            table[2, 1] = 8;
+
+            //loop through and print the whole 2D array
+            for (int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < columns; y++)
+
+                {
+                    Console.WriteLine(table[x, y]);
+                }
+
+                Console.WriteLine(); //a new line is created after each row is iterated
+            }
         }
     }
 }
