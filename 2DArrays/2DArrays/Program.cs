4775c09d57893c13f49fbab5d7d688a2c5ef7e6e
cmt1 <cmt1@protonmail.com>
2020-07-14 20:18:09 -0700

    update

---
+++ 2DArrays/2DArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 3;
+            int colums = 3;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
