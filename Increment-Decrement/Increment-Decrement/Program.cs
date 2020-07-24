c04632b40d87a9d52c01917d00f6a51fd5be32e0
cmt1 <cmt1@protonmail.com>
2020-07-23 17:35:57 -0700

    H0P03_Part2

---
+++ Increment-Decrement/Increment-Decrement/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args) //method
+        {
+            for (int a = 0; a <= 10; a++) // this loop is saying that a is 0 and to print out numbers less than or equal to 10 
+                                          //the a++ is saying to increment by 1 
+
+            {
+                Console.WriteLine(a);
+
+            }
+             Console.WriteLine("Challenge 3 Below");
+            {
+                for (int b = 15; b >= 1; b--)
+                    Console.WriteLine(b);
+            } 
+
+        }
+
+
+    }
+}
+    
+    
+    
+            
+        
+    
+
