ac5b022235fff60264ae22098a404adfcb5a4cd7
cmt1 <cmt1@protonmail.com>
2020-07-30 00:50:20 -0700

    HOP04

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Methods
+{
+    class Program
+    {
+        //Method called Mod that takes in 2 parameters
+        //both of type interger
+        
+        public static int Mod(int a, int b)
+        {
+            //the passes in intergers are divided
+            //and the result is stored in the integer "modulus"
+            
+             //return the values stored in "modulus"
+
+            int modulus = a % b;
+            return modulus;
+        }
+        //Method called "Main",
+        //Called whent the program is ran
+        static void Main(string[] args)
+        {
+            // call methid "Adding" and pass in 2 parameter: 20 and 13
+            //print the results of method "Mod"
+           
+            Console.WriteLine(Mod(20, 13));
+        }
+    }
+}
