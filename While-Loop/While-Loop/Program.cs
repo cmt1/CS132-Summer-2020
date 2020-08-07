34e2ca638786a1b98858334348a6adeea7d1dba1
cmt1 <cmt1@protonmail.com>
2020-08-06 22:39:01 -0700

    HOP05-2

---
+++ While-Loop/While-Loop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            Console.WriteLine("Guess a number (1-10): ");
+            int userInput = Convert.ToInt32(Console.ReadLine()); // recieve uset input in "userInput" variable
+           
+            //challenge to change while loop
+            while (true)
+            {
+                if (userInput > 0 || userInput >= 10)
+
+                {
+                    if (userInput == num)
+                    {
+                        Console.WriteLine("Your guess was correct");
+                        break;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number must be between 1 - 10. Guess again: ");
+
+                       userInput = Convert.ToInt32(Console.ReadLine());
+
+                    }
+
+                }
+            }
+        }
+    }
+}
