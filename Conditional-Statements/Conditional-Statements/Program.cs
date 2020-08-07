83f0ee9a5c326a0927505d99bf78f7c6bb52b5f4
cmt1 <cmt1@protonmail.com>
2020-08-06 19:15:12 -0700

    HOP05-1

---
+++ Conditional-Statements/Conditional-Statements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            Console.WriteLine("Guess a number (1-10): ");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //recieve user input ans store in "userInput" cariable
+            //Challenge
+            if (userInput <= num)
+            {
+                Console.WriteLine("Your guess was correct!");
+            }
+            else
+            {
+                Console.WriteLine("Number must be between 1-10!!!");
+            }
+            }
+        }
+    }
+
