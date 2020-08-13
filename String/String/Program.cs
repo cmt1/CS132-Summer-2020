36a93c0bc92bf0ed03f47183e6c59e47f2abcdb4
cmt1 <cmt1@protonmail.com>
2020-08-13 00:05:58 -0700

    HOP_06

---
+++ String/String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS132";
+            string message = Welcome + CourseName;
+
+            for (int i = 0; i < message.Length; i = i+2) //Challenge
+            {
+              
+
+                
+               
+                     
+                Console.WriteLine(message[i]);
+            }
+           
+        }
+    }
+}
