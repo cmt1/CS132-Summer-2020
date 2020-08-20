e12c27bdbaee1cff2eb9acea60bd46e392d7864e
cmt1 <cmt1@protonmail.com>
2020-08-20 01:24:05 -0700

    HOP07

---
+++ Date-Time/Date-Time/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        void GetPersonalInfo()
+        {
+            Console.WriteLine("Enter Your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What year were you born?: ");
+            int birthYear = Convert.ToInt32(Console.ReadLine());
+
+            int age = (Convert.ToInt32(DateTime.Now.Year) - birthYear);
+
+            Console.WriteLine("Here are what we know from your inputs: ");
+            Console.WriteLine("Your name: " + name);
+            Console.WriteLine("Today is: " + DateTime.Now.ToShortDateString());
+            Console.WriteLine("Your age: " + age);
+
+            //Challenge
+            int daysNum = 31;
+            DateTime dt = DateTime.Now;
+            Console.Write(dt.ToString("MM" + " "));
+            Console.Write(dt.ToString(daysNum + " "));
+            Console.ReadLine();
+
+        }
+        static void Main(string[] ags)
+        {
+            Program MyProgram = new Program();
+
+            MyProgram.GetPersonalInfo();
+        }
+    }
+}
