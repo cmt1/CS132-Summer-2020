4848819fc56f4dfca5cb092700f32b50ab277434
cmt1 <cmt1@protonmail.com>
2020-09-04 00:45:59 -0700

    HOP_10

---
+++ Generic-Collection/Generic-Collection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Generic_Collection
+{
+    class Program
+    {
+        class StoreAnything <T, U, V> 
+        {
+            public T var1 { set; get; }
+            public U var2 { set; get; }
+
+            //Challenge
+            public V var3 { set; get; }
+        }
+
+        
+        static void Main(string[] args)
+        {
+        StoreAnything<string, int, char> MyVar1 = new StoreAnything<string, int, char> { var1 = "CS", var2 = 132, var3 = '!' };
+
+           
+            //Challenge added char
+            Console.WriteLine(MyVar1.var1 + " " + MyVar1.var2 + " " + MyVar1.var3);
+        }
+    }
+}
