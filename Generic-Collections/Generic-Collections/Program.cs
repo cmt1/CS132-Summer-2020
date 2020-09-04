e7dacb0675b6ea1b2cb245746d265a61de49aa68
cmt1 <cmt1@protonmail.com>
2020-09-04 01:18:25 -0700

    HOP_10

---
+++ Generic-Collections/Generic-Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace Generic_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable myHashTable = new Hashtable()
+            {
+                {"Kim", 26 },
+            { "John", 77},
+                {"Max", 50}
+            };
+
+            int HashTableVal1 = (int)myHashTable["Kim"];
+            int HashTableVal2 = (int)myHashTable["John"];
+            int HasshTableVal3 = (int)myHashTable["Max"];
+
+            Console.WriteLine(HashTableVal1);
+            Console.WriteLine(HashTableVal2);
+            Console.WriteLine(HasshTableVal3);
+
+        }
+    }
+}
