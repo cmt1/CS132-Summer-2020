ce8f6acbbb3d92152a76590b2b895a8cf66fcd44
cmt1 <cmt1@protonmail.com>
2020-09-04 01:03:44 -0700

    HOP_10

---
+++ Arraylist/Arraylist/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace Arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArrList = new ArrayList();
+
+            myArrList.Add(10);
+            myArrList.Add(11.5);
+            myArrList.Add("This is a string");
+
+            myArrList.AddRange(new ArrayList() { 20, 17.7, "This is another stiring" });
+
+            myArrList.Remove(10);
+
+            myArrList.RemoveAt(0);
+
+            myArrList.RemoveRange(1, 2);
+
+            foreach (var value in myArrList)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
